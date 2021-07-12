b2ba3de11e3ba7cd668e78a0f9d83b569a946352
sie-fufu <ca.fukuda@gmail.com>
2021-07-12 21:48:34 +0900

    イグノアされていたファイルをコミット

---
+++ public/scripts/login.js
@@ -0,0 +1,23 @@
+async () => {
+  const BASE_URL = "https://petit-practice.herokuapp.com/event";
+  const res = await fetch(BASE_URL);
+
+  const result = await res.json();
+  const authPassword = result[0].authPassword;
+  const adminPassword = result[0].adminPassword;
+
+  document.querySelector(".login").addEventListener("click", () => {
+    // ユーザーの入力を求める
+    const UserInput = prompt("パスワードを入力して下さい:", "");
+    // 入力チェック
+    if (/\d+/g.test(UserInput) === false) {
+      // 半角英数字以外の文字が存在したらエラー
+      alert("正しく、半角英数字のみを入力して下さい。");
+    } else if (UserInput === authPassword) {
+      // 入力内容からファイル名を生成して移動
+      location.href = "./main.html";
+    } else if (UserInput === adminPassword) {
+      location.href = "./decide.html";
+    }
+  });
+};
